efc459a228fc7d23ac52db0b927429c8ee68aee2
Mrigank Singh <mrigank.11.8@gmail.com>
2025-01-29 14:44:09 +0530

    new firebase

--- firebase.js
+++ firebase.js
@@ -5,13 +5,20 @@ import { getAuth } from 'firebase/auth';
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDVoLggLqOfezH3O5kUrrZ5nHBOYH7Q7KI",
-  authDomain: "safeher-489de.firebaseapp.com",
-  projectId: "safeher-489de",
-  storageBucket: "safeher-489de.appspot.com",
-  messagingSenderId: "738459165140",
-  appId: "1:738459165140:web:6befeff60f344470fdb641",
-  measurementId: "G-CHHKXHYEML"
+  apiKey: "AIzaSyAaar_mUUrR50scGLgGUP3C6n4c8jO7-ic",
+
+  authDomain: "safeher-d647d.firebaseapp.com",
+
+  projectId: "safeher-d647d",
+
+  storageBucket: "safeher-d647d.firebasestorage.app",
+
+  messagingSenderId: "229117584279",
+
+  appId: "1:229117584279:web:e6f66b64fa97f8841edad2",
+
+  measurementId: "G-RZMTMEM1CJ"
+
 };
 
 // Initialize Firebase
